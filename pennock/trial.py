8d937aa55c715f837e31c4d0fde7acb51bd9c747
ishikarsan <130214455+ishikarsan@users.noreply.github.com>
2023-04-08 09:53:39 +0100

    initial

---
+++ pennock/trial.py
@@ -0,0 +1,11 @@
+lists=['help no']
+cooler = []
+
+def splitting (thing):
+    
+    cool = thing.split(',')
+    cooler = cool.append(i)
+    return cooler
+
+maybe = splitting(lists)
+print (maybe)
