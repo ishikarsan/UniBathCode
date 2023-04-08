3923d002c77e0077ced115bfc9d346e076478621
ishikarsan <130214455+ishikarsan@users.noreply.github.com>
2023-04-08 11:19:26 +0100

    blocks

--- pennock/ReadCascadeFile.py
+++ pennock/ReadCascadeFile.py
@@ -6,7 +6,27 @@ def ReadCascadeFile(inputFile):
     outputCheck = 0
     cascadeFile = []
     
-    with open("inputFile",'r') as f:
-        [cascadeFile.append(line) for line in f.readlines()]
+    with open(inputFile,'r') as f:
+        for line in f.readlines():
+            if "<CIRCUIT>" in line:
+                circuitCheck = 1
+            if "</CIRCUIT>" in line:
+                circuitCheck = circuitCheck + 1
+            if "<TERMS>" in line:
+                termsCheck = 1
+            if "</TERMS>" in line:
+                termsCheck = termsCheck + 1
+            if "<OUTPUT>" in line:
+                outputCheck = 1
+            if "</OUTPUT>" in line:
+                outputCheck = outputCheck + 1
+            if "#" in line:
+                continue
+            else:
+                cascadeFile.append(line)
+    if circuitCheck %2 != 0:
+        returnDict = {cascadeFile}
 
 
+
+ReadCascadeFile("file.txt")
